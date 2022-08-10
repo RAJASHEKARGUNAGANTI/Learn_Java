dd3d266e850706b7cd76d211cfbdd986c3c89d75
sheelisowmya <sheelisowmya@gmail.com>
2022-08-10 06:49:01 -0700

    hello java

---
+++ Largest.java
@@ -0,0 +1,9 @@
+package Learn_Java;
+
+public class Largest {
+    public static void main(String[] args) {
+        int a=15;
+        int b=10;
+        System.out.println(a+"is greater than"+b);
+    }
+}
